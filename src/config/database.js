c97d15b744dbb050f693a2350138f73e4b37888a
geisondossantos <geison_bf@hotmail.com>
2022-06-13 18:49:11 -0300

    Adiciona SSL as configurações

--- src/config/database.js
+++ src/config/database.js
@@ -16,6 +16,10 @@ module.exports = {
   },
   dialectOptions: {
     timezone: 'America/Sao_Paulo',
+    ssl: {
+      require: true,
+      rejectUnauthorized: false,
+    },
   },
   timezone: 'America/Sao_Paulo',
 };
--- src/database/index.js
+++ src/database/index.js
@@ -7,5 +7,14 @@ import Foto from '../models/Foto';
 const models = [Aluno, User, Foto];
 const connection = new Sequelize(databaseConfig);
 
+connection
+  .authenticate()
+  .then(() => {
+    console.log('Connection has been established successfully.');
+  })
+  .catch((err) => {
+    console.error('Unable to connect to the database:', err);
+  });
+
 models.forEach((model) => model.init(connection));
 models.forEach((model) => model.associate && model.associate(connection.models));
