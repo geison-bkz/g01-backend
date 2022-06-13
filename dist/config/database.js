b9066a0f7fa1628d2df5674475fe40a845d41244
geisondossantos <geison_bf@hotmail.com>
2022-06-13 20:59:48 -0300

    Adiciona dist

---
+++ dist/config/database.js
@@ -0,0 +1,25 @@
+"use strict";require('dotenv').config();
+
+module.exports = {
+  dialect: 'postgres',
+  host: process.env.PGHOST,
+  port: process.env.PGPORT,
+  username: process.env.PGUSER,
+  password: process.env.PGPASSWORD,
+  database: process.env.PGDATABASE,
+  define: {
+    timestamps: true,
+    underscored: true,
+    underscoredAll: true,
+    createdAt: 'created_at',
+    updatedAt: 'updated_at',
+  },
+  dialectOptions: {
+    timezone: 'America/Sao_Paulo',
+    ssl: {
+      require: true,
+      rejectUnauthorized: false,
+    },
+  },
+  timezone: 'America/Sao_Paulo',
+};
---
+++ dist/database/seeds/20220529174403-criar-usuarios.js
@@ -0,0 +1,12 @@
+"use strict";module.exports = {
+  async up(queryInterface) {
+    await queryInterface.bulkInsert('People', [{
+      name: 'John Doe',
+      isBetaMember: false,
+    }], {});
+  },
+
+  async down(queryInterface) {
+    await queryInterface.bulkDelete('People', null, {});
+  },
+};
